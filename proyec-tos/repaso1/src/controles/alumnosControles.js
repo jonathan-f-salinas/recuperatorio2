57b4f9a59c3b9396ea51c8dca5b8afae857eb251
jonathan salinas <sameksalinas@gmail.com>
2023-06-01 01:42:11 -0300

    creacion de busqueda por dni

--- proyec-tos/repaso1/src/controles/alumnosControles.js
+++ proyec-tos/repaso1/src/controles/alumnosControles.js
@@ -3,5 +3,15 @@ const alumnos= require ('../../datos/alumnos.json')
 const todosAlumnos = (req, res)=>{
     res.status(200).json(alumnos)
 }
+const soloDni = (req,res)=>{
+    const dni = req.params.dni
+    const indice= alumnos.findIndex(a=>a.dni==dni)
+    
+    if (indice>=0){
+        res.status(200).json(alumnos[indice])
+    }else {
+        res.status(404).json("el alumno no fue encontrado")
+    }
+}
 
-module.exports= {todosAlumnos}
+module.exports= {todosAlumnos, soloDni}
--- proyec-tos/repaso1/src/rutas/alumnosrutas.js
+++ proyec-tos/repaso1/src/rutas/alumnosrutas.js
@@ -3,6 +3,7 @@ const rutas= express.Router();//a express le pido una ruta
 const alumnosControles= require ('../controles/alumnosControles')
 
 rutas.get('/',alumnosControles.todosAlumnos)// llamo a la funcion 
+rutas.get('/:dni',alumnosControles.soloDni)
 
 module.exports = {rutas}
 
